c1d135b80a8cb4b16bcb3103abb5452a4308f78a
Rex Park <haroo0310@gmail.com>
2022-01-05 19:17:23 +0900

    add First Files
    Former-commit-id: 6a6be00a68ea2213f98f50d3fbd41a7ff0c037ea

--- Library/Il2cppBuildCache/Android/arm64-v8a/il2cppOutput/lumpedcpp/Lump_libil2cpp_mono.cpp
+++
@@ -1,7 +0,0 @@
-#include "il2cpp-config.h"
-#include "D:\unity\2020.3.24f1\Editor\Data\il2cpp\libil2cpp\mono\MonoPosixHelper.cpp"
-#include "D:\unity\2020.3.24f1\Editor\Data\il2cpp\libil2cpp\mono\ThreadPool\ThreadPoolMonitorThread.cpp"
-#include "D:\unity\2020.3.24f1\Editor\Data\il2cpp\libil2cpp\mono\ThreadPool\ThreadPoolWorkerThread.cpp"
-#include "D:\unity\2020.3.24f1\Editor\Data\il2cpp\libil2cpp\mono\ThreadPool\threadpool-ms-io-poll.cpp"
-#include "D:\unity\2020.3.24f1\Editor\Data\il2cpp\libil2cpp\mono\ThreadPool\threadpool-ms-io.cpp"
-#include "D:\unity\2020.3.24f1\Editor\Data\il2cpp\libil2cpp\mono\ThreadPool\threadpool-ms.cpp"
